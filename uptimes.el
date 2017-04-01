3fdb0b23b424ce929c72b05ad29be4c5277f032c
Dave Pearson <davep@davep.org>
2017-04-01 11:25:21 +0100

    v3.2

--- uptimes.el
+++ uptimes.el
@@ -2,7 +2,7 @@
 ;; Copyright 1999-2017 by Dave Pearson <davep@davep.org>
 
 ;; Author: Dave Pearson <davep@davep.org>
-;; Version: 3.1
+;; Version: 3.2
 ;; Keywords: uptime
 ;; URL: https://github.com/davep/uptimes.el
 ;; Package-Requires: ((cl-lib "0.5"))
